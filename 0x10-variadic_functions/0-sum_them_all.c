a3ee6acbe4cc1f490c0b57e9d9c2b3971ed72a20
Getnetsh <Getnetss2009@gmail.com>
2023-10-12 06:09:54 -0700

    update

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,30 +1,25 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 
 /**
-*sum_them_all - sums all its parameters
-*@n: first parameter
-*
-*Return: 0 if n is null
-* or sum of parameters in other case
-*/
+ * sum_them_all -  adds all the parameters passed to it
+ * @n: number of args passed
+ *
+ * Return: sum of all the parameters
+ */
 
 int sum_them_all(const unsigned int n, ...)
 {
+	int sum = 0;
+	unsigned int i;
+	va_list params;
 
-va_list li;
-int sum = 0;
-unsigned int i;
+	va_start(params, n);
+	if (n != 0)
+	{
+		for (i = 0; i < n; i++)
+			sum += va_arg(params, int);
+	}
+	va_end(params);
 
-va_start(li, n);
-
-if (n != 0)
-{
-for (i = 0; i < n; i++)
-sum += va_arg(li, int);
-}
-
-va_end(li);
-return (sum);
-
+	return (sum);
 }
